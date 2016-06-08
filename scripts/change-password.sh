e3632270b89e030e86ddd140411b40e57a4fa3f0
khanhnhat <nguyen.k21@gmail.com>
2016-06-08 15:33:38 -0400

    Update email and password in user scripts

--- scripts/change-password.sh
+++ scripts/change-password.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl --include --request PATCH http://localhost:3000/change-password/$ID \
-  --header "Authorization: Token token=$TOKEN" \
+curl --include --request PATCH http://localhost:3000/change-password/1 \
+  --header "Authorization: Token token=BAhJIiU3NWIyZjZiZGNmOGY5OGJjMzU1MWVhMGZjNmQxMDJkNQY6BkVG--267fe26806f889af1e0eebefb21c026a9237d3b7" \
   --header "Content-Type: application/json" \
   --data '{
     "passwords": {
