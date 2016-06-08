f3fe2ce15a73ba5155d3ae9b66d3ed89e9f582c6
khanhnhat <nguyen.k21@gmail.com>
2016-06-08 15:43:43 -0400

    Write script to show all lists

---
+++ scripts/list/show-lists.sh
@@ -0,0 +1,2 @@
+curl --silent --request GET http://localhost:3000/lists \
+  --header "Authorization: Token token=BAhJIiU3NWIyZjZiZGNmOGY5OGJjMzU1MWVhMGZjNmQxMDJkNQY6BkVG--267fe26806f889af1e0eebefb21c026a9237d3b7"
