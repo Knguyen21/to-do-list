c7cc0c3d1febd694ef9b99e0f558deac47bd5744
khanhnhat <nguyen.k21@gmail.com>
2016-06-08 15:57:48 -0400

    Write script to delete a list

---
+++ scripts/list/delete-list.sh
@@ -0,0 +1,2 @@
+curl --include --request DELETE http://localhost:3000/lists/1 \
+--header "Authorization: Token token=BAhJIiU3NWIyZjZiZGNmOGY5OGJjMzU1MWVhMGZjNmQxMDJkNQY6BkVG--267fe26806f889af1e0eebefb21c026a9237d3b7" \
